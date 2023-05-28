3dc0a3500a951c1e9fd6909afce4917ec2e2eb0b
Андрей <d89284146236sv@gmail.com>
2023-05-28 20:18:52 +0300

    проверьте дз

--- src/test/java/com/example/homework2_13/DepartmentServiceTest.java
+++ src/test/java/com/example/homework2_13/DepartmentServiceTest.java
@@ -22,100 +22,130 @@ import java.util.stream.Stream;
 
 @ExtendWith(MockitoExtension.class)
 public class DepartmentServiceTest {
+
     @Mock
     private EmployeeService employeeService;
+
     @InjectMocks
     private DepartmentService departmentService;
-    private List<Employee> employees;
 
-    public static Stream<Arguments> employeeWithMaxWageTestParams() {
+    public static Stream<Arguments> getMaxWageTestParams() {
         return Stream.of(
                 Arguments.of(1, new Employee("Михаил", "Викторович", 1, 6000)),
-                Arguments.of(2, new Employee("Антонина", "Викторовна", 2, 3000))
+                Arguments.of(2, new Employee("Антонина", "Викторовна", 2, 3000)),
+                Arguments.of(3, new Employee("Людмила", "Викторовна", 3, 1000))
         );
     }
 
-    public static Stream<Arguments> employeeWithMinWageTestParams() {
+    public static Stream<Arguments> getMinWageTestParams() {
         return Stream.of(
                 Arguments.of(1, new Employee("Михаил", "Викторович", 1, 6000)),
-                Arguments.of(2, new Employee("Дмитрий", "Викторович", 2, 2000))
+                Arguments.of(2, new Employee("Дмитрий", "Викторович", 2, 2000)),
+                Arguments.of(3, new Employee("Людмила", "Викторовна", 3, 1000))
         );
     }
 
-    public static Stream<Arguments> employeeAllInDepartmentTestParams() {
+    public static Stream<Arguments> getAllInDepartTestParams() {
         return Stream.of(
-                Arguments.of(
-                        1,
+                Arguments.of(1,
                         List.of(
                                 new Employee("Михаил", "Викторович", 1, 6000)
                         )
                 ),
-                Arguments.of(
-                        2,
+                Arguments.of(2,
                         List.of(
-                                new Employee("Дмитрий", "Викторович", 2, 2000),
-                                new Employee("Антонина", "Викторовна", 2, 3000)
+                                new Employee("Антонина", "Викторовна", 2, 3000),
+                                new Employee("Дмитрий", "Викторович", 2, 2000)
                         )
                 ),
-                Arguments.of(
-                        4,
+                Arguments.of(3,
+                        List.of(
+                                new Employee("Людмила", "Викторовна", 3, 1000)
+                        )
+                ),
+                Arguments.of(4,
                         Collections.emptyList()
                 )
+
         );
     }
+
 
     @BeforeEach
     public void beforeEach() {
-        employees = List.of(
-                new Employee("Михаил", "Викторович", 1, 6000),
-                new Employee("Антонина", "Викторовна", 2, 3000),
-                new Employee("Дмитрий", "Викторович", 2, 2000)
+        List<Employee> employees = List.of(
+                new Employee("Михаил",
+                        "Викторович",
+                        1,
+                        6000),
+                new Employee("Антонина",
+                        "Викторовна",
+                        2,
+                        3000),
+                new Employee("Дмитрий",
+                        "Викторович",
+                        2,
+                        2000),
+                new Employee("Людмила",
+                        "Викторовна",
+                        3,
+                        1000)
         );
-        Mockito.when(employeeService.getAll()).thenReturn(employees);
+        Mockito.when(employeeService.list()).thenReturn(employees);
     }
+
     @ParameterizedTest
-    @MethodSource("employeeWithMaxWageTestParams")
-    public void employeeWithMaxWageTest(int departmentId, Employee expected) {
+    @MethodSource("getMaxWageTestParams")
+    public void getMaxWageTest(int departmentId, Employee expected) {
         Assertions.assertThat(departmentService.getMaxWage(departmentId))
                 .isEqualTo(expected);
     }
+
     @Test
-    public void employeeWithMaxWageNotFoundTest() {
+    public void getMaxWageTest2() {
         Assertions.assertThatExceptionOfType(EmployeeNotFoundException.class)
                 .isThrownBy(() -> departmentService.getMaxWage(4));
     }
+
     @ParameterizedTest
-    @MethodSource("employeeWithMinWageTestParams")
-    public void employeeWithMinWageTest(int departmentId, Employee expected) {
-        Assertions.assertThat(departmentService.getMinWage(departmentId))
+    @MethodSource("getMinWageTestParams")
+    public void getMinWageTest(int department, Employee expected) {
+        Assertions.assertThat(DepartmentService.getMinWage(department))
                 .isEqualTo(expected);
     }
+
     @Test
-    public void employeeWithMinWageNotFoundTest() {
+    public void getMinWageTest2() {
         Assertions.assertThatExceptionOfType(EmployeeNotFoundException.class)
-                .isThrownBy(() -> departmentService.getMinWage(4));
+                .isThrownBy(() -> DepartmentService.getMinWage(4));
     }
+
     @ParameterizedTest
-    @MethodSource("employeeAllInDepartmentTestParams")
-    public void employeeAllInDepartTest(int departmentId, List<Employee> expected) {
-        Assertions.assertThat(departmentService.getAllInDepart())
+    @MethodSource("getAllInDepartTestParams")
+    public void getAllInDepartTest(int departmentId, List<Employee> expected) {
+        Assertions.assertThat(DepartmentService.getAllInDepart(departmentId))
                 .containsExactlyInAnyOrderElementsOf(expected);
     }
-    @Test
-    public void employeeAllByDepartTest() {
-        Map<Integer, List<Employee>> expected = Map.of(
-                        1,
-                        List.of(
-                                new Employee("Михаил", "Викторович", 1, 6000)
-                        ),
 
-                        2,
-                        List.of(
-                                new Employee("Дмитрий", "Викторович", 2, 2000),
-                                new Employee("Антонина", "Викторовна", 2, 3000)
-                        )
+    @Test
+    public void getAllByDepartTestParams() {
+        Map<Integer, List<Employee>> expected = Map.of(
+                1,
+                List.of(
+                        new Employee("Михаил", "Викторович", 1, 6000)
+                ),
+                2,
+                List.of(
+                        new Employee("Антонина", "Викторовна", 2, 3000),
+                        new Employee("Дмитрий", "Викторович", 2, 2000)
+                ),
+                3,
+                List.of(
+                        new Employee("Людмила", "Викторовна", 3, 1000)
+                )
         );
-        Assertions.assertThat(departmentService.getAllByDepart())
-                .containsExactlyInAnyOrder((Employee) expected);
+
+        Assertions.assertThat(DepartmentService.getAllByDepart())
+                .containsExactlyInAnyOrderElementsOf((Iterable<? extends Employee>) expected);
     }
 }
